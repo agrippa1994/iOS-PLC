019235399f658316c0639c73095dccb262807233
agrippa1994 <agrippa1994@icloud.com>
2015-08-06 22:12:43 +0200

    Some updates on the UI (main tab bar controller added)

---
+++ PLC/MainTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarController.swift
+//  PLC
+//
+//  Created by Manuel Stampfl on 06.08.15.
+//  Copyright (c) 2015 mani1337. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
